71149af88cf6bbc2b092f5e1546a4baf0d31c734
suada-a <suada.abdurahim@gmail.com>
2024-04-19 12:38:21 -0400

    Started exercise1.1

---
+++ part1/src/App.jsx
@@ -0,0 +1,26 @@
+const Hello = (props) => {
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
+const App = () => {
+  const friends = [
+    { name: 'Peter', age: 4 },
+    { name: 'Maya', age: 10}
+  ]
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <div>
+      <p>{friends[0].name} {friends[1].age}</p>
+      <p>{friends[1].name} {friends[1].age}</p>
+    </div>
+  )
+}
+
+export default App
