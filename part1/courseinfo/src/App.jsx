0f5358774e5439d32f77c14eaf3e68885e2e3304
suada-a <suada.abdurahim@gmail.com>
2024-04-28 22:40:08 +0000

    Finish Exercises 2.2-2.3: Course Information

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,13 +1,13 @@
-const Total = (props) => {
-  return <p>Number of exercises {props.sumOfExercises}</p>
+const Total = ({ parts }) => {
+  const sumOfExercises = parts.reduce((previousExercises, currentExercises) => {
+    return previousExercises + currentExercises.exercises
+  }, 0)
+
+  return <p>Number of exercises {sumOfExercises}</p>
 }
 
 const Part = ({ name, exercises }) => {
-  return (
-    <p>
-      {name} {exercises}
-    </p>
-  )
+  return <p>{name} {exercises}</p>
 }
 
 const Content = ({ parts }) => {
@@ -20,19 +20,19 @@ const Content = ({ parts }) => {
   )
 }
 
-const Header = (props) => {
-  return <h1>{props.course}</h1>
+const Header = ({ course }) => {
+  return <h1>{course}</h1>
 }
 
-const Course = ( {course} ) => {
+const Course = ({ course }) => {
   return (
     <>
-      <Header course={course.name}/>
+      <Header course={course.name} />
       <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </>
   )  
 }
-
 
 const App = () => {
   const course = {
